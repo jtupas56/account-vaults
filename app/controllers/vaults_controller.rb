8af012ade5dab3dc9787f14ad21e3b5613913d51
joshuatupas <joshuatupas@gmail.com>
2026-07-27 17:48:49 +0100

    UI, add search and URL validation implemented

--- app/controllers/vaults_controller.rb
+++ app/controllers/vaults_controller.rb
@@ -2,31 +2,34 @@ class VaultsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_vault, only: %i[show edit update destroy]
 
-  # GET /vaults
-# GET /vaults
-def index
-  @vaults = current_user.vaults.order(updated_at: :desc)
-end
-  # GET /vaults/1
+  def index
+    @vaults = current_user.vaults
+    if params[:search].present?
+      search_term = "%#{params[:search]}%"
+      @vaults = @vaults.where(
+        "site ILIKE :q OR url ILIKE :q OR login ILIKE :q",
+        q: search_term
+      )
+    end
+    @vaults = @vaults.order(updated_at: :desc)
+  end
+
   def show
   end
 
-  # GET /vaults/new
   def new
     @vault = current_user.vaults.build
   end
 
-  # GET /vaults/1/edit
   def edit
   end
 
-  # POST /vaults
   def create
     @vault = current_user.vaults.build(vault_params)
 
     respond_to do |format|
       if @vault.save
-format.html { redirect_to vaults_path, notice: "Vault was successfully created." }
+        format.html { redirect_to vaults_path, notice: "Vault was successfully created." }
         format.json { render :show, status: :created, location: @vault }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -35,11 +38,10 @@ format.html { redirect_to vaults_path, notice: "Vault was successfully created."
     end
   end
 
-  # PATCH/PUT /vaults/1
   def update
     respond_to do |format|
       if @vault.update(vault_params)
-format.html { redirect_to vaults_path, notice: "Vault was successfully updated.", status: :see_other }
+        format.html { redirect_to vaults_path, notice: "Vault was successfully updated.", status: :see_other }
         format.json { render :show, status: :ok, location: @vault }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -48,7 +50,6 @@ format.html { redirect_to vaults_path, notice: "Vault was successfully updated."
     end
   end
 
-  # DELETE /vaults/1
   def destroy
     @vault.destroy!
 
@@ -60,12 +61,10 @@ format.html { redirect_to vaults_path, notice: "Vault was successfully updated."
 
   private
 
-  # SECURITY FIX: user can only access THEIR vaults
   def set_vault
     @vault = current_user.vaults.find(params[:id])
   end
 
-  # Strong parameters
   def vault_params
     params.require(:vault).permit(:site, :url, :login, :password)
   end
--- app/models/vault.rb
+++ app/models/vault.rb
@@ -2,7 +2,7 @@ class Vault < ApplicationRecord
   belongs_to :user
 
   validates :site, presence: true
-  validates :url, presence: true
+  validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "must be a valid URL" }
   validates :login, presence: true
   validates :password, presence: true
 end
